50daceb27e0a0dd22116cac3a5a7b43441923841
BoBkiNN_ <bobkin.yangirov@gmail.com>
2022-07-04 13:07:14 +0600

    b4

--- src/main/java/xyz/bobkinn_/collectibilites/Handler.java
+++ src/main/java/xyz/bobkinn_/collectibilites/Handler.java
@@ -26,6 +26,7 @@ public class Handler implements Listener {
         assert meta != null;
         meta.addEnchant(Enchantment.MENDING,1,true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        meta.setCustomModelData(1000000);
         meta.setDisplayName("§b§lНеизвестная карта");
         meta.setLore(Arrays.asList("§eНажмите ПКМ для открытия", "описание"));
         cardItem.setItemMeta(meta);
@@ -33,17 +34,16 @@ public class Handler implements Listener {
 
 
         if (clItem.getType() == Material.DIAMOND){
-            if (p.getInventory().getItemInMainHand().getAmount() >= 1){
-                p.getInventory().getItemInMainHand().setAmount(p.getInventory().getItemInMainHand().getAmount()-1);
-            } else {
-                p.getInventory().remove(Material.DIAMOND);
-            }
-            event.getPlayer().getInventory().setItemInMainHand(cardItem);
+            int amountOld = p.getInventory().getItemInMainHand().getAmount();
+            p.getInventory().getItemInMainHand().setAmount(amountOld-1);
+            event.getPlayer().getInventory().addItem(cardItem);
         }
         if (clItem.isSimilar(cardItem)){
             int var = Utils.randInt(0,5);
+
             p.sendMessage(var+" card");
-            p.sendMessage("Это карта");
+            int amountOld = p.getInventory().getItemInMainHand().getAmount();
+            p.getInventory().getItemInMainHand().setAmount(amountOld-1);
         }
     }
 }
