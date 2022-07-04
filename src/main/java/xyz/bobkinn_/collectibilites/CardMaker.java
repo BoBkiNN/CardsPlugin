cfbfa8f57b134208e284c23f6ecc321ea2258092
BoBkiNN_ <bobkin.yangirov@gmail.com>
2022-07-04 14:19:56 +0600

    b11

--- src/main/java/xyz/bobkinn_/collectibilites/CardMaker.java
+++ src/main/java/xyz/bobkinn_/collectibilites/CardMaker.java
@@ -1,11 +1,13 @@
 package xyz.bobkinn_.collectibilites;
 
-
+import org.json.simple.*;
 
 import java.util.ArrayList;
 
 public class CardMaker {
     public static ArrayList<Card> getCommon(){
 
+
+        return null;
     }
 }
--- src/main/java/xyz/bobkinn_/collectibilites/Main.java
+++ src/main/java/xyz/bobkinn_/collectibilites/Main.java
@@ -2,12 +2,29 @@ package xyz.bobkinn_.collectibilites;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        try {
+            getDataFolder().mkdir();
+            File cardsFile =  new File(getDataFolder(),"cards.json");
+            cardsFile.createNewFile();
+            InputStream jarStream = getResource("cards.json");
+            OutputStream fileStream;
+            fileStream = new FileOutputStream(new File(getDataFolder(),"cards.json"));
+            IOUtils.copy(jarStream,fileStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        getConfig();
+
         new MainConfig(getPlugin(Main.class));
         Bukkit.getPluginManager().registerEvents(new Handler(),this);
         getLogger().info("Enabled!");
